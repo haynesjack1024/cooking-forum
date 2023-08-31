9cec79e8be09b77bfaf0437283a3d3451bed3273
haynesjack1024 <jackie2018@protonmail.com>
2023-08-31 13:26:20 +0200

    added a basic layout with global styles

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,11 +1,20 @@
 export const metadata = {
-  title: 'Cooking Forum'
-}
+  title: "Cooking Forum",
+};
+
+import './global.scss';
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <header>
+          <h1>Cooking Forum</h1>
+        </header>
+        <div id="box">
+          <main>{children}</main>
+        </div>
+      </body>
     </html>
-  )
+  );
 }
