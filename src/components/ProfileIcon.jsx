7ba18d82c90b2ce083839690b850fcbef4487102
haynesjack1024 <jackie2018@protonmail.com>
2023-09-15 10:08:07 +0200

    adjusted the profile icon attribute names

--- src/components/ProfileIcon.jsx
+++ src/components/ProfileIcon.jsx
@@ -7,14 +7,14 @@ export default function ProfileIcon() {
       role="img"
     >
       <title>profile icon</title>
-      <g stroke="black" stroke-width="2.5" fill="transparent">
+      <g stroke="black" strokeWidth="2.5" fill="transparent">
         <circle cx="50%" cy="50%" r="11.25" />
         <circle cx="50%" cy="9.5" r="3.5" />
         <circle
           cx="50%"
           cy="25.5"
           r="9"
-          clip-path="circle(11.25 at 50% 50%) view-box"
+          clipPath="circle(11.25 at 50% 50%) view-box"
         />
       </g>
     </svg>
